8204044d92428792cf40be74b4f24be616272593
Luka Popovic <lukapopovic0111@gmail.com>
2025-05-02 23:41:01 +0200

    Editing dice

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const currentScorePlayerOne = document.querySelector('#current-score-1');
 const currentScorePlayerTwo = document.querySelector('#current-score-2');
 const totalScorePlayerOne = document.querySelector('#score-1');
 const totalScorePlayerTwo = document.querySelector('#score-2');
-const dice = document.querySelector('#dice');
+const dice = document.querySelectorAll('.dice');
 //BTN
 const btnNewGame = document.querySelector('#btn-new-game');
 const btnRollDice = document.querySelector('#btn-roll-dice');
 const btnHold = document.querySelector('#btn-hold');
 
-let random = Math.trunc(Math.random() * 6) + 1;
+let random;
+let randomTwo;
 let scorePlayerOne;
 let scorePlayerTwo;
+let scorePlayerOneTotal;
+let scorePlayerTwoTotal;
+let activPlayer = 'player 1';
+
+btnRollDice.addEventListener('click', function () {
+  random = Math.trunc(Math.random() * 6) + 1;
+  dice.forEach(function (dicee) {
+    dicee.src = `dice-${random}.png`;
+    dicee.classList.remove('hidden');
+  });
+});
