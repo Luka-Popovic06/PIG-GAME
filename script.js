736dc2c1c79140ebda42f615fbe6e0843c02e7d4
Luka Popovic <lukapopovic0111@gmail.com>
2025-05-04 14:44:00 +0200

    BTN-roll dice

--- script.js
+++ script.js
@@ -4,23 +4,29 @@ const currentScorePlayerTwo = document.querySelector('#current-score-2');
 const totalScorePlayerOne = document.querySelector('#score-1');
 const totalScorePlayerTwo = document.querySelector('#score-2');
 const dice = document.querySelectorAll('.dice');
+const playerBoxOne = document.querySelector('.player-box-1');
+const playerBoxTwo = document.querySelector('.player-box-2');
 //BTN
 const btnNewGame = document.querySelector('#btn-new-game');
 const btnRollDice = document.querySelector('#btn-roll-dice');
 const btnHold = document.querySelector('#btn-hold');
 
 let random;
-let randomTwo;
-let scorePlayerOne;
-let scorePlayerTwo;
-let scorePlayerOneTotal;
-let scorePlayerTwoTotal;
+let randomValue;
+let scorePlayerOne = 0;
+let scorePlayerTwo = 0;
+let scorePlayerOneTotal = 0;
+let scorePlayerTwoTotal = 0;
 let activPlayer = 'player 1';
 
 btnRollDice.addEventListener('click', function () {
   random = Math.trunc(Math.random() * 6) + 1;
-  dice.forEach(function (dicee) {
-    dicee.src = `dice-${random}.png`;
-    dicee.classList.remove('hidden');
+  activPlayerSelection();
+  scoreing();
+  coloring();
+  whoWin();
+  dice.forEach(function (dices) {
+    dices.src = `dice-${random}.png`;
+    dices.classList.remove('hidden');
   });
 });
